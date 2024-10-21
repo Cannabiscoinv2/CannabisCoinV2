26c52eb0a7773e999eb4be4950b5a94bf359caa5
Hser2bio <hser2bio@icloud.com>
2024-10-21 12:53:32 +0200

    IsInitialBlockDownload

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -466,8 +466,8 @@ UniValue getblocktemplate(const UniValue& params, bool fHelp)
     if (vNodes.empty())
         throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "CannabisCoin is not connected!");
 
-    if (IsInitialBlockDownload())
-        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "CannabisCoin is downloading blocks...");
+    /*if (IsInitialBlockDownload())
+        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "CannabisCoin is downloading blocks...");*/
 
     static unsigned int nTransactionsUpdatedLast;
 
